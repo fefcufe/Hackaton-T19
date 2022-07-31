8d596b2a244f868e023859d73b617b39e867d29a
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-07-31 06:45:34 -0300

    fix: :memo: realiza alguns ajustes e adiciona imagem(seção feminina)

--- wine-trybe/src/components/Womens.jsx
+++ wine-trybe/src/components/Womens.jsx
@@ -1,32 +1,50 @@
 import { womanApi } from "../services/apiWine";
-import "./WomensCard.css";
+import "../style/WomensCard.css";
 
 export default function Womens() {
   return (
-    <div className="woman-container">
-      {womanApi.map((wine) => (
-        <div key={wine.id} className="card-woman-container">
-          <h2 className="wine-title">{wine.title}</h2>
-          <div className="text-pic-container">
-            <img src={wine.image} alt={wine.title} className="wine-picture" />
-            <p className="wine-text">{wine.comment}</p>
-          </div>
-          <div className="price-container">
-            <div className="socio-container">
-              <p>SÓCIO WINE: R$</p>
-              <p className="wine-price-socio">{(wine.priceMember).toFixed(2)}</p>
-              <div className="btt-add">
-              <button type="button" className="btt-style">Adicionar</button>
+    <div>
+      <div className="title-img-container">
+        <h2 className="title-womens">Uma seção dedicada a ELAS...</h2>
+        <div className="img-womens-container">
+          <img
+            src="https://i0.wp.com/www.wine.com.br/winepedia/wp-content/uploads/2021/12/vinhos-mulheres.png?resize=1024%2C628&ssl=1"
+            alt="mulheres brindando"
+            className="img-womens"
+          ></img>
+        </div>
+      </div>
+
+      <div className="woman-container">
+        {womanApi.map((wine) => (
+          <div key={wine.id} className="card-woman-container">
+            <h2 className="wine-title">{wine.title}</h2>
+            <div className="text-pic-container">
+              <img src={wine.image} alt={wine.title} className="wine-picture" />
+              <p className="wine-text">{wine.comment}</p>
+            </div>
+            <div className="price-container">
+              <div className="socio-container">
+                <p>SÓCIO WINE: R$</p>
+                <p className="wine-price-socio">
+                  {wine.priceMember.toFixed(2)}
+                </p>
+                <div className="btt-add">
+                  <button type="button" className="btt-style">
+                    Adicionar
+                  </button>
+                </div>
+              </div>
+              <div className="nao-socio-container">
+                <p>NÃO SÓCIO: R$</p>
+                <p className="wine-price-nao-socio">
+                  {wine.priceNmember.toFixed(2)}
+                </p>
               </div>
             </div>
-            <div className="nao-socio-container">
-              <p>NÃO SÓCIO: R$</p>
-              <p className="wine-price-nao-socio">{(wine.priceNmember).toFixed(2)}</p>
-              
-            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
