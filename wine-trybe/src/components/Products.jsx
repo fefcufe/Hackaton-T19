e4d3eaf4fb981ab071e4ee0a06c7175a735e9ec6
Fernanda Ferreira <fefcufe@gmail.com>
2022-08-15 19:00:06 -0300

    fix: ajusta import css

--- wine-trybe/src/components/Products.jsx
+++ wine-trybe/src/components/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { apiProducts } from "../services/api";
-import "./Products.css";
+import "../style/Products.css";
 
 import "../style/Products.css"
 
